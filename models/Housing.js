bc7a388145f9ef0d9fc3a78bab5624b29629f276
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2022-02-21 17:37:23 +0200

    Add user and housing models validation.

--- models/Housing.js
+++ models/Housing.js
@@ -1,17 +1,34 @@
 const { Schema, model, Types: { ObjectId } } = require('mongoose');
 
+const URL_PATTERN = /^https?:\/\/(.+)/;
+
 const houseSchema = new Schema({
-    name: { type: String, required: true },
-    type: { type: String, enum: ['Apartment', 'Villa', 'House'], required: true },
-    year: { type: Number, required: true },
-    city: { type: String, required: true },
-    homeImg: { type: String, required: true },
-    description: { type: String, required: true },
-    pieces: { type: Number, required: true },
+    name: { type: String, required: [true, 'Name is required!'], minlength: [6, 'Name should be at least 6 characters!'] },
+    type: { type: String, enum: ['Apartment', 'Villa', 'House'], required: [true, 'Type should be - Apartment, Villa or House.'] },
+    year: {
+        type: Number, required: [true, 'Year is required!'],
+        min: [1850, 'Year should be between 1850 and 2021'],
+        max: [2021, 'Year should be between 1850 and 2021']
+    },
+    city: { type: String, required: [true, 'City is required!'], minlength: [4, 'City should be at least 4 characters long'] },
+    homeImg: {
+        type: String, required: [true, 'Image is required!'], validate: {
+            validator(value) {
+                return URL_PATTERN.test(value);
+            },
+            message: 'Home image should be starts with http:// or https://'
+        }
+    },
+    description: { type: String, required: [true, 'Description is required!'], maxlength: [60, 'Property description should be a maximum of 60 characters long.'] },
+    pieces: {
+        type: Number, required: [true, 'Available Pieces are required'],
+        min: [0, 'Available Pieces should be positive number from 0 to 10'],
+        max: [10, 'Available Pieces should be positive number from 0 to 10']
+    },
     owner: { type: ObjectId, ref: 'User', required: true },
     renters: { type: [ObjectId], ref: 'User', default: [] },
 },
-{timestamps: true});
+    { timestamps: true });
 
 const Housing = model('Housing', houseSchema);
 
--- models/User.js
+++ models/User.js
@@ -1,15 +1,17 @@
 const { Schema, model, Types: { ObjectId } } = require('mongoose');
 
-// TODO change user model
-// TODO add validation
-
-// const URL_PATTERN = /^https?:\/\/(.+)/;
-// const NAME_PATTERN = /^[a-zA-Z]+$/;
-// const EMAIL_PATTERN = /^([a-zA-Z]+)@([a-zA-Z]+)\.([a-zA-Z]+)$/;
+const NAME_PATTERN = /^[A-Z][a-z]+ [A-Z][a-z]+$/;
 
 const userSchema = new Schema({
-    name: { type: String, required: [true, 'Full name is required!'] },
-    username: { type: String, required: [true, 'Username is required!'] },
+    name: {
+        type: String, required: [true, 'Full name is required!'], validate: {
+            validator(value) {
+                return NAME_PATTERN.test(value);
+            },
+            message: 'Valid full name is required!'
+        }
+    },
+    username: { type: String, required: [true, 'Username is required!'], minlength: [5, 'The username should be at least 5 characters long!'] },
     hashedPassword: { type: String, required: true }
 });
 
